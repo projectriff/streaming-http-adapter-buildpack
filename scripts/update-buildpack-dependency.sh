cf2b2d0a08df4ae5aa8f9700f4956784a30a400e
Ben Hale <bhale@vmware.com>
2020-10-13 10:50:22 -0700

    Initial Pipelines
    This change adds the Octo-pipelines including dependency updates for all the
    buildpack's dependencies.
    
    Signed-off-by: Ben Hale <bhale@vmware.com>

--- scripts/update-buildpack-dependency.sh
+++
@@ -1,7 +0,0 @@
-uri() {
-  sed 's|gs://|https://storage.googleapis.com/|' "${ROOT}"/dependency/url
-}
-
-sha256() {
-  shasum -a 256 "${ROOT}"/dependency/streaming-http-adapter-linux-amd64-*.tgz | cut -f 1 -d ' '
-}
